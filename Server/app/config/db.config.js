b2196f67473f31816d1970957ca2391838c4091f
Aurangzeb <aurangzeb.khan@informatik.hs-fulda.de>
2021-11-09 21:04:20 +0100

    MyUniShop Ecommerce App

---
+++ Server/app/config/db.config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+
+module.exports = {
+  HOST: 'localhost',
+  USER: "root",
+  PASSWORD: "password",
+  DB: "myunishop_db"
+};
+
+// module.exports = {
+//   HOST: process.env.DB_HOST,
+//   USER: process.env.DB_USER,
+//   PASSWORD: process.env.DB_PASSWORD,
+//   DB: process.env.DB_NAME,
+// };
